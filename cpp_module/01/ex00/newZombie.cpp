103ef8c73f19a089edab206a6b00ef988e378466
eelijus <yoagi110@gmail.com>
2022-04-01 09:21:42 +0900

    cpp00 done

--- cpp_module/01/ex00/newZombie.cpp
+++ cpp_module/01/ex00/newZombie.cpp
@@ -6,13 +6,13 @@
 /*   By: sujilee <sujilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 17:14:14 by sujilee           #+#    #+#             */
-/*   Updated: 2022/03/30 17:15:05 by sujilee          ###   ########.fr       */
+/*   Updated: 2022/04/01 09:10:43 by sujilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie	*newZombie(std::string name) {
-	Zombie *ptr = new Zombie(name);
-	return ptr;
+	Zombie *babyZombie = new Zombie(name);
+	return babyZombie;
 }
